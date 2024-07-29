2e54d7b70812fbed5a05ddabb70bf752e79a281d
jqn1 <154387579+jqn1@users.noreply.github.com>
2024-07-29 18:15:24 -0400

    Draw grid

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
+let rows = 20;
+let cols = 20;
+let grid = [];
+let cellSize;
+
 function setup() {
     createCanvas(400, 400);
-}
+    cellSize = width/cols;
+    for (let i = 0; i<rows; i++) {
+            grid.push(Array());
+            for (let j = 0; j<cols; j++) {
+                let x = (width/cols)*j;
+                let y = (height/rows)*i;
+                grid[i][j] = new Cell(x,y);
+            }
+        }
+    }
   
 function draw() {
     background(220);
+    for(row of grid) {
+        for(cell of row) {
+            cell.display();
+        }
+
+    }
 }
